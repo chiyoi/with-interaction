25158a8232770967c1abfd883b032ecbebc04b90
chiyoi <chiyoi2140@outlook.com>
2023-12-29 16:10:39 +0800

    fix

--- src/form/index.ts
+++ src/form/index.ts
@@ -1,4 +1,4 @@
-export function PayloadJSONWithFiles(payload: string, files: { blob: Blob, filename: string }[]) {
+export function PayloadJSONWithFiles(payload: {}, files: { blob: Blob, filename: string }[]) {
   const form = new FormData()
   form.append('payload', new Blob([JSON.stringify(payload)], { type: 'application/json' }))
   for (const i in files) form.append(`files[${i}]`, files[i].blob, files[i].filename)
